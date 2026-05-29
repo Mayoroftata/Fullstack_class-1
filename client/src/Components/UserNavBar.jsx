003560260f019732d300c3c88e05d81c32833040
Mayoroftata <objmak@gmail.com>
2026-05-29 20:03:04 +0100

    Add logout functionality to UserNavBar and enhance UI elements

--- client/src/Components/UserNavBar.jsx
+++ client/src/Components/UserNavBar.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import { Link} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const UserNavBar = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate('/signin');
+  };
+
   return (
     <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container fw-bolder">
           <Link className="navbar-brand" to="/">
             MayorTech
@@ -21,7 +28,7 @@ const UserNavBar = () => {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto">
+            <ul className="navbar-nav ms-auto align-items-center">
               <li className="nav-item">
                 <Link className="nav-link" to="/">
                   Home
@@ -37,10 +44,29 @@ const UserNavBar = () => {
                   Contact
                 </Link>
               </li>
+              <li className="nav-item ms-3 d-flex align-items-center gap-2">
+                <button
+                  type="button"
+                  className="btn btn-light rounded-circle p-2 d-flex align-items-center justify-content-center"
+                  aria-label="User profile"
+                  style={{ width: '38px', height: '38px' }}
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
+                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 1 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+                  </svg>
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-outline-light btn-sm"
+                  onClick={handleLogout}
+                >
+                  Log Out
+                </button>
+              </li>
             </ul>
           </div>
         </div>
-    </nav>
+      </nav>
     </div>
   )
 }
